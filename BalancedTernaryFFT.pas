d4288f392e4261ccc30ab9fb762f14348bee33a1
nabbla [work] <nabbla@yandex.ru>
2012-03-22 13:48:02 +0400

    v. 0.1

--- BalancedTernaryFFT.pas
+++ BalancedTernaryFFT.pas
@@ -1,5 +1,6 @@
 unit BalancedTernaryFFT;
-//v. 0.01
+
+//Version 0.1
 
 interface
 uses math;
@@ -25,6 +26,40 @@ type
       procedure inverseFFT;
       constructor Create;
   end;
+
+  ComplexTernary2D=class
+    private
+      Re_data, Im_data: array of Real; //адресацию сделаем свою
+      _qx,_qy: Integer; //число тритов по ос€м x и y
+      _Tx,_Ty,_Nx,_Ny: Integer; //полное число элементов и мин/макс значение (-N, N)
+      _T: Integer; //всего элементов в массиве
+      base: Integer; //смещение отсчета (0,0)
+      function Re_value(x,y: Integer): Real;
+      function Im_value(x,y: Integer): Real;
+      procedure set_Re(x,y: Integer; value: Real);
+      procedure set_Im(x,y: Integer; value: Real);
+      procedure Inversion(fbase,mult,_T1: Integer);
+      procedure general_FFT(fbase,mult,_T1: Integer; inverse: boolean);
+    public
+      procedure Set_Length(X,Y: Integer);
+      procedure FFT;
+      procedure InverseFFT;
+      procedure power_spectrum;
+      procedure Clear;
+
+      property Nx: Integer read _Nx;
+      property Ny: Integer read _Ny;
+      property Tx: Integer read _Tx;
+      property Ty: Integer read _Ty;
+      property T: Integer read _T;
+      property Re[x,y: Integer]: Real read Re_value write Set_Re;
+      property Im[x,y: Integer]: Real read Im_value write Set_Im;
+
+
+      constructor Create;
+  end;
+
+
 
 implementation
 
@@ -470,8 +505,350 @@ begin
     incr:=big_incr;
   end;
 
+end;
+
+
+function ComplexTernary2D.Re_value(x,y: Integer): Real;
+begin
+  Assert(x<=_Nx,'Re_value: x too big');
+  Assert(x>=-_Nx,'Re_value: x too small');
+  Assert(y<=_Ny,'Re_value: y too big');
+  Assert(y>=-_Ny,'Re_value: y too small');
+  Re_value:=Re_data[base+y*_Tx+x];
+end;
+
+function ComplexTernary2D.Im_value(x,y: Integer): Real;
+begin
+  Assert(x<=_Nx,'Im_value: x too big');
+  Assert(x>=-_Nx,'Im_value: x too small');
+  Assert(y<=_Ny,'Im_value: y too big');
+  Assert(y>=-_Ny,'Im_value: y too small');
+  Im_value:=Im_data[base+y*_Tx+x];
+end;
+
+procedure ComplexTernary2D.set_Re(x,y: Integer; value: Real);
+begin
+  Assert(x<=_Nx,'set_Re: x too big');
+  Assert(x>=-_Nx,'set_Re: x too small');
+  Assert(y<=_Ny,'set_Re: y too big');
+  Assert(y>=-_Ny,'set_Re: y too small');
+  Re_data[base+y*_Tx+x]:=value;
+end;
+
+procedure ComplexTernary2D.set_Im(x,y: Integer; value: Real);
+begin
+  Assert(x<=_Nx,'set_Im: x too big');
+  Assert(x>=-_Nx,'set_Im: x too small');
+  Assert(y<=_Ny,'set_Im: y too big');
+  Assert(y>=-_Ny,'set_Im: y too small');
+  Im_data[base+y*_Tx+x]:=value;
+end;
+
+procedure ComplexTernary2D.Set_Length(x,y: Integer);
+begin
+  assert(x>-1,'Set_Length: negative argument x');
+  assert(y>-1,'Set_Length: negative argument y');
+  if x<1 then begin
+    _qx:=-1;
+    _Tx:=0;
+    _Nx:=-1;
+  end
+  else begin
+    _qx:=math.Ceil(ln(x)/ln(3));
+    _Tx:=Round(power(3,_qx));
+    _Nx:=(_Tx-1) div 2;
+  end;
+
+  if y<1 then begin
+    _qy:=-1;
+    _Ty:=0;
+    _Ny:=-1;
+  end
+  else begin
+    _qy:=math.Ceil(ln(y)/ln(3));
+    _Ty:=Round(power(3,_qy));
+    _Ny:=(_Ty-1) div 2;
+  end;
+  _T:=_Tx*_Ty;
+  SetLength(Re_data,_T);
+  SetLength(Im_data,_T);
+  base:=(_T-1) div 2;
+end;
+
+constructor ComplexTernary2D.Create;
+begin
+  inherited Create;
+  Set_Length(0,0);
+end;
+
+procedure ComplexTernary2D.general_FFT(fbase,mult,_T1: Integer; Inverse: boolean);
+var N1,M1,T1,k,j,incr,big_incr,i: Integer;
+  sqrt3,Wr,Wi,Ph,incWr,incWi,TwoPi,tmpWr: Real;
+  //W - фазовый множитель, r,i - действ. и мнимое знач.
+  xsum,ysum,xdif,ydif,ax,ay,xp1,xm1,yp1,ym1,x0,y0: Real;
+  //sum - суммы
+  //dif - разности
+  //p1,0,m1 - +1,0,-1 соотв
+begin
+  sqrt3:=-sqrt(3)/2;
+  TwoPi:=2*pi;
+  if Inverse then begin
+    sqrt3:=-sqrt3;
+    TwoPi:=-TwoPi;
+  end;
+  inversion(fbase,mult,_T1);
+  if _T1<2 then exit;
+  //перва€ итераци€ - без фазовых множителей, она сама€ быстра€
+  T1:=_T1;
+  N1:=(T1-1) div 2;
+  incr:=mult;
+
+  while N1>0 do begin
+    T1:=T1 div 3;
+    N1:=(T1-1) div 2;
+    big_incr:=incr*3; //дл€ внутреннего цикла
+    M1:=(incr-1) div (2*mult); //дл€ внешнего
+    //отдельно обработаем i=0, там фазовый множ. не нужен
+    for k:=-N1 to N1 do begin
+       j:=fbase+big_incr*k;
+       //отдельно обраб. нулевое значение - там не нужно фаз. множителей
+        x0:=Re_data[j];
+        y0:=Im_data[j];
+        j:=j+incr;
+        xp1:=Re_data[j];
+        yp1:=Im_data[j];
+        j:=j-2*incr;
+        xm1:=Re_data[j];
+        ym1:=Im_data[j];
+
+        xsum:=xp1+xm1;
+        ysum:=yp1+ym1;
+        ydif:=sqrt3*(xp1-xm1);
+        xdif:=sqrt3*(ym1-yp1);
+        // 4 сложени€ и 2 умножени€ (с плав. точкой)
+        Ax:=x0-0.5*xsum;
+        Ay:=y0-0.5*ysum;
+        // 6 сложений и 4 умножени€
+        //сейчас j указывает на -1-й элемент
+        Re_data[j]:=Ax-xdif;
+        Im_data[j]:=Ay-ydif;
+
+        j:=j+2*incr;
+        //+1-й элемент
+        Re_data[j]:=Ax+xdif;
+        Im_data[j]:=Ay+ydif;
+
+        j:=j-incr;
+        //0-й элемент
+        Re_data[j]:=x0+xsum;
+        Im_data[j]:=y0+ysum;
+
+        //итого, 12 сложений и 4 умножени€
+       end;
+    //шаг фазового множител€: 2pi/incr;
+    //на первой итерации просто 2pi, но там цикл и не запуститс€
+    //на второй итер:
+    Ph:=TwoPi/big_incr*mult;
+    incWr:=cos(Ph);
+    incWi:=-sin(Ph);
+    Wr:=1;
+    Wi:=0;
+    for i:=1 to M1 do begin
+      //пересчитываем фазовый множитель, потом делаем циклы дл€ i и -i
+      tmpWr:=Wr;
+      Wr:=tmpWr*incWr-Wi*incWi;
+      Wi:=Wi*incWr+tmpWr*incWi;
+      for k:=-N1 to N1 do begin
+        //итераци€ дл€ +i
+        j:=fbase+i*mult+big_incr*k;
+       //x0,y0 - без изменений
+        x0:=Re_data[j];
+        y0:=Im_data[j];
+        j:=j+incr;
+        //а здесь надо умножить на фаз. множ.
+        //элем. +1 - на W
+        tmpWr:=Re_data[j];
+        yp1:=Im_data[j];
+
+        xp1:=tmpWr*Wr-yp1*Wi;
+        yp1:=yp1*Wr+tmpWr*Wi;
+
+        j:=j-2*incr;
+        //элем. -1 умножаем на W* (сопр€ж)
+        tmpWr:=Re_data[j];
+        ym1:=Im_data[j];
+
+        xm1:=tmpWr*Wr+ym1*Wi;
+        ym1:=ym1*Wr-tmpWr*Wi;
+
+        xsum:=xp1+xm1;
+        ysum:=yp1+ym1;
+        ydif:=sqrt3*(xp1-xm1);
+        xdif:=sqrt3*(ym1-yp1);
+        // 4 сложени€ и 2 умножени€ (с плав. точкой)
+        Ax:=x0-0.5*xsum;
+        Ay:=y0-0.5*ysum;
+        // 6 сложений и 4 умножени€
+        //сейчас j указывает на -1-й элемент
+        Re_data[j]:=Ax-xdif;
+        Im_data[j]:=Ay-ydif;
+
+        j:=j+2*incr;
+        //+1-й элемент
+        Re_data[j]:=Ax+xdif;
+        Im_data[j]:=Ay+ydif;
+
+        j:=j-incr;
+        //0-й элемент
+        Re_data[j]:=x0+xsum;
+        Im_data[j]:=y0+ysum;
+
+        //“еперь, то же самое дл€ элемента -i
+
+        j:=fbase-i*mult+big_incr*k;
+       //x0,y0 - без изменений
+        x0:=Re_data[j];
+        y0:=Im_data[j];
+        j:=j+incr;
+        //а здесь надо умножить на фаз. множ.
+        //элем. +1 - на W* (т.к -i)
+        tmpWr:=Re_data[j];
+        yp1:=Im_data[j];
+
+        xp1:=tmpWr*Wr+yp1*Wi;
+        yp1:=yp1*Wr-tmpWr*Wi;
+
+        j:=j-2*incr;
+        //элем. -1 умножаем на W
+        tmpWr:=Re_data[j];
+        ym1:=Im_data[j];
+
+        xm1:=tmpWr*Wr-ym1*Wi;
+        ym1:=ym1*Wr+tmpWr*Wi;
+
+        xsum:=xp1+xm1;
+        ysum:=yp1+ym1;
+        ydif:=sqrt3*(xp1-xm1);
+        xdif:=sqrt3*(ym1-yp1);
+        // 4 сложени€ и 2 умножени€ (с плав. точкой)
+        Ax:=x0-0.5*xsum;
+        Ay:=y0-0.5*ysum;
+        // 6 сложений и 4 умножени€
+        //сейчас j указывает на -1-й элемент
+        Re_data[j]:=Ax-xdif;
+        Im_data[j]:=Ay-ydif;
+
+        j:=j+2*incr;
+        //+1-й элемент
+        Re_data[j]:=Ax+xdif;
+        Im_data[j]:=Ay+ydif;
+
+        j:=j-incr;
+        //0-й элемент
+        Re_data[j]:=x0+xsum;
+        Im_data[j]:=y0+ysum;
+
+
+       end;
+    end;
+    //конец одного сло€
+    incr:=big_incr;
+  end;
 
 end;
+
+procedure ComplexTernary2D.FFT;
+var x,y: Integer;
+begin
+//сначала одномерный FFT по каждой строке
+  for y:=-_Ny to _Ny do begin
+    general_FFT(base+y*_Tx,1,_Tx,false);
+  end;
+//теперь по каждому столбцу
+  for x:=-_Nx to _Nx do begin
+    general_FFT(base+x,_Tx,_Ty,false);
+  end;
+
+end;
+
+procedure ComplexTernary2D.InverseFFT;
+var x,y: Integer;
+begin
+
+//по каждому столбцу, предварительно поделив на _T
+  for x:=-_Nx to _Nx do begin
+    for y:=-_Ny to _Ny do begin
+      set_Re(x,y,Re_value(x,y)/_T);
+      set_Im(x,y,Im_value(x,y)/_T);
+    end;
+    general_FFT(base+x,_Tx,_Ty,true);
+  end;
+
+  for y:=-_Ny to _Ny do begin
+    general_FFT(base+y*_Tx,1,_Tx,true);
+  end;
+
+end;
+
+procedure ComplexTernary2D.inversion(fbase,mult,_T1: integer);
+var i,j,k,b,q,_qmin1,_N: Integer;
+    trits: array of Integer;
+    tmp: Real;
+begin
+  if _T1<4 then Exit;
+  q:=ceil(ln(_T1)/ln(3));
+  SetLength(trits,q);
+  _qmin1:=q-1;
+  _N:=(_T1-1) div 2;
+  for j:=0 to _qmin1 do trits[j]:=-1; //самый отрицательный элемент
+  for i:=-_N to _N do begin
+    k:=0;
+    b:=1;
+    for j:=_qmin1 downto 0 do begin
+      k:=k+trits[j]*b;
+      b:=b*3;
+    end;
+    //k указывает инверсию от i.
+    if k>i then begin
+    //помен€ем местами
+      tmp:=Re_data[fbase+k*mult];
+      Re_data[fbase+k*mult]:=Re_data[fbase+i*mult];
+      Re_data[fbase+i*mult]:=tmp;
+      tmp:=Im_data[fbase+k*mult];
+      Im_data[fbase+k*mult]:=Im_data[fbase+i*mult];
+      Im_data[fbase+i*mult]:=tmp;
+    end;
+    //прибавим единичку
+    j:=0;
+    while j<q do begin
+      inc(trits[j]);
+      if trits[j]<2 then break;
+      trits[j]:=-1;
+      inc(j);
+    end;
+
+  end;
+end;
+
+procedure ComplexTernary2D.power_spectrum;
+var x,y: Integer;
+begin
+  for y:=-_Ny to _Ny do
+    for x:=-_Nx to _Nx do
+      Set_Re(x,y,Sqr(Re_value(x,y))+Sqr(Im_value(x,y)));
+end;
+
+procedure ComplexTernary2D.Clear;
+var x: Integer;
+begin
+  for x:=0 to _T-1 do begin
+    Re_data[x]:=0;
+    Im_data[x]:=0;
+  end;
+end;
+
+
+
 
 
 
